f348362506acd152053b0f8ff7cd78cf02599524
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2020-12-08 11:59:15 +0200

    week 2 assignement 1 task 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card,CardImg,CardText,CardBody,CardTitle,Breadcrumb,BreadcrumbItem,} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish }) {
   if (dish != null)
@@ -17,12 +18,12 @@ function RenderDish({ dish }) {
   else return <div></div>;
 }
 
-function RenderComments({ dish }) {
-  if (dish != null)
+function RenderComments({ comments }) {
+  if (comments != null)
     return (
       <div className="col-12 col-md-5 m-1">
         <h4> Comments </h4>
-        {dish.comments.map((item, key) => (
+        {comments.map((item, key) => (
           <ul className="list-unstyled">
             <li>{item.comment}</li>
             <li>
@@ -44,15 +45,24 @@ const Dishdetail = (props) => {
   return (
     <div className="container">
       <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/menu">Menu</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
         <div className="col-12">
-          <h3>{props.name}</h3>
+          <h3>{props.dish.name}</h3>
           <hr />
         </div>
       </div>
       <div className="row">
-        <RenderDish dish={props.dish} />
-
-        <RenderComments dish={props.dish} />
+        <div className="col-12 col-md-5 m-1">
+          <RenderDish dish={props.dish} />
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <RenderComments comments={props.comments} />
+        </div>
       </div>
     </div>
   );
