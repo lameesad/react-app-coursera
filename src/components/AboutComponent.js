596a561e3ef6eeb75aa6c650c17c6ed64a7a4052
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2020-12-09 11:14:24 +0200

    Controlled Forms

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,32 +1,36 @@
 import React from "react";
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media,} from "reactstrap";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody,
+  CardHeader,
+  Media,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
 function RenderLeader({ leader }) {
   if (leader != null)
     return (
-            <div key={leader.id} className="col-12 mt-5">
-            <Media tag="li" >
-                <Media left middle>
-                    <Media object src={leader.image} alt={leader.name} >
-
-                    </Media>
-                </Media>
-                <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                </Media>
-            </Media>
-  </div>
-     
+      <div key={leader.id} className="col-12 mt-5">
+        <Media tag="li">
+          <Media left middle>
+            <Media object src={leader.image} alt={leader.name}></Media>
+          </Media>
+          <Media body className="ml-5">
+            <Media heading>{leader.name}</Media>
+            <p>{leader.designation}</p>
+            <p>{leader.description}</p>
+          </Media>
+        </Media>
+      </div>
     );
   else return <div></div>;
 }
 
 function About(props) {
   const leaders = props.leaders.map((leader) => {
-    return <RenderLeader leader = {leader} />
+    return <RenderLeader leader={leader} />;
   });
 
   return (
